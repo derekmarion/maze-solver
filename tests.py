25336f382fd2a5c850b54d994ff9832ecd84f762
Derek Marion <derek.j.marion@gmail.com>
2024-10-02 21:05:57 -0700

    Add tests for _break_entrance_and_exit method

--- tests.py
+++ tests.py
@@ -26,6 +26,14 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(m1._cells), num_rows)
         self.assertEqual(len(m1._cells[0]), num_cols)
 
+    def test_break_entrance_and_exit(self):
+        num_cols = 120
+        num_rows = 100
+        m1 = Maze(Point(0, 0), num_rows, num_cols, 10, 10, _win=self.win, _render=False)
+        m1._break_entrance_and_exit()
+        self.assertIs(m1._cells[0][0].has_top_wall, False)
+        self.assertIs(m1._cells[-1][-1].has_bottom_wall, False)
+
 
 if __name__ == "__main__":
     unittest.main()
